dd9a4f55e8adde3e9e233b7ba845d607e8d80625
瘴地怪鼠 <yan.yajie@outlook.com>
2022-03-30 06:12:40 +0000

    上传NPU版本程序部分

---
+++ TensorFlow/contrib/graph/SRNET_ID1089_for_TensorFlow/code_npu_version/cfg.py
@@ -0,0 +1,83 @@
+"""
+SRNet - Editing Text in the Wild
+Some configurations.
+Copyright (c) 2019 Netease Youdao Information Technology Co.,Ltd.
+Licensed under the GPL License (see LICENSE for details)
+Written by Yu Qian
+"""
+
+import os
+import argparse
+import moxing as mox
+
+# pretrained vgg
+vgg19_weights = r'/home/ma-user/modelarts/user-job-dir/code/model_logs/vgg19/vgg19_weights_tf_dim_ordering_tf_kernels_notop.pb'
+
+# model parameters
+lt = 1.
+lt_alpha = 1.
+lb = 1.
+lb_beta = 10.
+lf = 1.
+lf_theta_1 = 10.
+lf_theta_2 = 1.
+lf_theta_3 = 500.
+epsilon = 1e-8
+
+# train
+learning_rate = 1e-4 # default 1e-3
+decay_rate = 0.9
+decay_steps = 10000
+staircase = False
+beta1 = 0.9 # default 0.9
+beta2 = 0.999 # default 0.999
+max_iter = 500000
+show_loss_interval = 50
+write_log_interval = 50
+save_ckpt_interval = 10000
+gen_example_interval = 1000
+checkpoint_savedir = '/cache/out/model_logs/checkpoints'
+tensorboard_dir = '/cache/out/model_logs/train_logs'
+pretrained_ckpt_path = None
+train_name = None # used for name examples and tensorboard logdirs, set None to use time
+
+# data
+batch_size = 8
+data_shape = [64, None]
+data_dir = '/cache/data'
+i_t_dir = 'i_t'
+i_s_dir = 'i_s'
+t_sk_dir = 't_sk'
+t_t_dir = 't_t'
+t_b_dir = 't_b'
+t_f_dir = 't_f'
+mask_t_dir = 'mask_t'
+example_data_dir = r'/home/ma-user/modelarts/user-job-dir/code/examples/labels'
+example_result_dir = '/cache/out/genLogs'
+
+# predict
+predict_ckpt_path = None
+predict_data_dir = None
+predict_result_dir = 'examples/result'
+
+#out_dir = ''
+
+def main():
+    code_dir = os.path.dirname(__file__)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train_url", type=str)
+    parser.add_argument("--data_url", type=str)
+    parser.add_argument("--modelarts_data_dir", type=str, default="/cache/data")
+    parser.add_argument("--modelarts_output_dir", type=str, default="/cache/out")
+    paraConfig = parser.parse_args()
+
+    #out_dir = paraConfig.modelarts_output_dir
+    os.makedirs(paraConfig.modelarts_data_dir)
+    os.makedirs(paraConfig.modelarts_output_dir)
+    os.makedirs(r'/cache/out/genLogs')
+    mox.file.copy_parallel(src_url=paraConfig.data_url, dst_url=paraConfig.modelarts_data_dir)
+    os.system("python3" + ' ' + code_dir + '/train.py')
+
+if __name__ == "__main__":
+    main()
---
+++ TensorFlow/contrib/graph/SRNET_ID1089_for_TensorFlow/code_npu_version/train.py
@@ -0,0 +1,105 @@
+"""
+SRNet - Editing Text in the Wild
+Model training.
+Copyright (c) 2019 Netease Youdao Information Technology Co.,Ltd.
+Licensed under the GPL License (see LICENSE for details)
+Written by Yu Qian
+"""
+import argparse
+import moxing as mox
+
+from npu_bridge.npu_init import *
+
+import tensorflow as tf
+from model import SRNet
+import numpy as np
+import os
+import cfg
+from utils import *
+from datagen import srnet_datagen, get_input_data
+
+from tensorflow.core.protobuf.rewriter_config_pb2 import  RewriterConfig
+
+def main():
+    # define train_name
+    if not cfg.train_name:
+        train_name = get_train_name()
+    else: 
+        train_name = cfg.train_name
+    
+    # define model
+    print_log('model compiling start.', content_color = PrintColor['yellow'])
+    model = SRNet(shape = cfg.data_shape, name = train_name)
+    print_log('model compiled.', content_color = PrintColor['yellow'])
+
+    # define data generator
+    #datagen()之中包含yield，所以不会正真执行，而是返回一个生成器(迭代器)
+    gen = srnet_datagen()
+    
+    with model.graph.as_default():
+        init = tf.global_variables_initializer()
+        trainCfg = tf.ConfigProto()
+        custom_op = trainCfg.graph_options.rewrite_options.custom_optimizers.add()
+        custom_op.name = "NpuOptimizer"
+        trainCfg.graph_options.rewrite_options.remapping = RewriterConfig.OFF
+
+        #trainCfg.graph_options.rewrite_options.memory_optimization = RewriterConfig.OFF
+        #custom_op.parameter_map["dynamic_input"].b = True
+
+        #custom_op.parameter_map["dynamic_graph_execute_mode"].s = tf.compat.as_bytes("dynamic_execute")
+        #custom_op.parameter_map["dynamic_inputs_shape_range"].s = tf.compat.as_bytes("data:[-1, 224, 224, 3], [-1, 224, 224, 3], [-1, 224, 224, 1], [-1, 224, 224, 3], [-1, 224, 224, 3], [-1, 224, 224, 3], [-1, 224, 224, 1]")
+
+
+        with tf.Session(config=npu_config_proto()) as sess:
+            saver = tf.train.Saver(tf.global_variables(), max_to_keep = 100)
+            
+            # load pretrained weights or initialize variables
+            if cfg.pretrained_ckpt_path:
+                print_log('weight loading start.', content_color = PrintColor['yellow'])
+                saver.restore(sess, cfg.pretrained_ckpt_path)
+                print_log('weight loaded.', content_color = PrintColor['yellow'])
+            else:
+                print_log('weight initialize start.', content_color = PrintColor['yellow'])
+                sess.run(init)
+                print_log('weight initialized.', content_color = PrintColor['yellow'])
+            
+            # train
+            print_log('training start.', content_color = PrintColor['yellow'])
+            for step in range(cfg.max_iter):
+                global_step = step + 1
+                
+                # train and get loss
+                d_loss, g_loss, d_log, g_log = model.train_step(sess, global_step, *next(gen))
+
+                # show loss
+                if global_step % cfg.show_loss_interval == 0 or step == 0:
+                    print_log ("step: {:>6d}   d_loss: {:>3.5f}   g_loss: {:>3.5f}".format(global_step, d_loss, g_loss))
+                
+                # write tensorboard
+                if global_step % cfg.write_log_interval == 0:
+                    write_summary(model.d_writer, model.g_writer, d_log, g_log, global_step)
+
+                # gen example
+                if global_step % cfg.gen_example_interval == 0:
+                    savedir = os.path.join(cfg.example_result_dir, train_name, 'iter-' + str(global_step).zfill(len(str(cfg.max_iter))))
+                    predict_data_list(model, sess, savedir, get_input_data())
+                    print_log ("example generated in dir {}".format(savedir), content_color = PrintColor['green'])
+
+                # save checkpoint
+                if global_step % cfg.save_ckpt_interval == 0:
+                    savedir = os.path.join(cfg.checkpoint_savedir, train_name, 'iter')
+                    if not os.path.exists(savedir):
+                        os.makedirs(savedir)
+                    save_checkpoint(sess, saver, savedir, global_step)
+                    print_log ("checkpoint saved in dir {}".format(savedir), content_color = PrintColor['green'])
+
+            print_log('training finished.', content_color = PrintColor['yellow'])
+            pb_savepath = os.path.join(cfg.checkpoint_savedir, train_name, 'final.pb')
+            save_pb(sess, pb_savepath, ['o_sk', 'o_t', 'o_b', 'o_f'])
+            print_log('pb model saved in dir {}'.format(pb_savepath), content_color = PrintColor['green'])
+
+    mox.file.copy_parallel(src_url = r'\cache\out', dst_url = r'obs:\\cann-nju-srnet\trainout')
+
+if __name__ == '__main__':
+    main()
+
