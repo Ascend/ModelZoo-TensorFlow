9ed0f90d0b9f8a2e974fe196549751574f5ad1f4
王宝弟 <wangbd20210604@163.com>
2022-03-31 06:26:36 +0000

    update

--- TensorFlow/built-in/cv/image_classification/MobileNetV1_ID0093_for_TensorFlow/train.py
+++ TensorFlow/built-in/cv/image_classification/MobileNetV1_ID0093_for_TensorFlow/train.py
@@ -124,7 +124,6 @@ def main(args):
         config_proto = tf.compat.v1.ConfigProto()
         custom_op = config_proto.graph_options.rewrite_options.custom_optimizers.add()
         custom_op.name = "NpuOptimizer"
-        custom_op.parameter_map["precision_mode"].s = tf.compat.as_bytes("allow_mix_precision")
         custom_op.parameter_map["use_off_line"].b = True
         custom_op.parameter_map["precision_mode"].s = tf.compat.as_bytes(args.precision_mode)
         config_proto.graph_options.rewrite_options.remapping = RewriterConfig.OFF
