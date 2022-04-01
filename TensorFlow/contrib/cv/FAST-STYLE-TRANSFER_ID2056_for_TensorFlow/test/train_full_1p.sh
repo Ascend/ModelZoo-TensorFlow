b170b3e898ecf48488a2766ff758e8672675f112
wu-lanchabu <1098900762@qq.com>
2022-04-01 06:36:32 +0000

    update TensorFlow/contrib/cv/FAST-STYLE-TRANSFER_ID2056_for_TensorFlow/test/train_full_1p.sh.

--- TensorFlow/contrib/cv/FAST-STYLE-TRANSFER_ID2056_for_TensorFlow/test/train_full_1p.sh
+++ TensorFlow/contrib/cv/FAST-STYLE-TRANSFER_ID2056_for_TensorFlow/test/train_full_1p.sh
@@ -75,6 +75,7 @@ etp_flag=${etp_running_flag}
 #fi
 echo ${print_log}
 
+batch_size=20
 CaseName=${Network}_bs${batch_size}_${RANK_SIZE}'p'_'acc'
 
 # 跳转到code目录
@@ -98,7 +99,7 @@ start_time=$(date +%s)
 # 您的训练数据集在${data_path}路径下，请直接使用这个变量获取
 # 您的训练输出目录在${output_path}路径下，请直接使用这个变量获取
 # 您的其他基础参数，可以自定义增加，但是batch_size请保留，并且设置正确的值
-batch_size=20
+
 
 if [ x"${etp_flag}" != x ];
 then
