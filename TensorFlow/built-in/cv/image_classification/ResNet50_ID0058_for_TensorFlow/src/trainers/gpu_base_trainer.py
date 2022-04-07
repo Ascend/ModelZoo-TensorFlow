8c197183a09b68b03900485cf7f1ad65b1527640
wangwei <641413440@qq.com>
2022-04-07 09:13:25 +0000

    update /ResNet50_ID0058_for_TensorFlow/src/trainers/gpu_base_trainer.py.

--- TensorFlow/built-in/cv/image_classification/ResNet50_ID0058_for_TensorFlow/src/trainers/gpu_base_trainer.py
+++ TensorFlow/built-in/cv/image_classification/ResNet50_ID0058_for_TensorFlow/src/trainers/gpu_base_trainer.py
@@ -109,7 +109,7 @@ class GPUBaseTrain(object):
                 iterations_per_loop=self.config['iterations_per_loop'], 
                 keep_checkpoint_max=5,
                 enable_small_channel=1,
-                modify_mixlist='ReduceMeanD.json')
+                modify_mixlist='./src/trainers/ReduceMeanD.json')
             else :
                 run_config = NPURunConfig(hcom_parallel=True, precision_mode="allow_mix_precision", save_summary_steps=0, log_step_count_steps=None, enable_data_pre_proc=True,save_checkpoints_secs=1e9, session_config=session_config, model_dir = self.config['model_dir'], iterations_per_loop=self.config['iterations_per_loop'])
 
