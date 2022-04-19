75a193619f12ffaf5df4dadeb34cca722c681d54
majun121 <867479212@qq.com>
2022-04-19 05:54:40 +0000

    update  WideDeep_ID2940_for_TensorFlow/test/train_performance_1p.sh.

--- TensorFlow/built-in/recommendation/WideDeep_ID2940_for_TensorFlow/test/train_performance_1p.sh
+++ TensorFlow/built-in/recommendation/WideDeep_ID2940_for_TensorFlow/test/train_performance_1p.sh
@@ -96,8 +96,8 @@ else
    mkdir -p $cur_path/output/$ASCEND_DEVICE_ID/ckpt
 fi
 
-export RANK_TABLE_FILE=$cur_path/../config/1p_$ASCEND_DEVICE_ID.json
-export RANK_ID=0
+#export RANK_TABLE_FILE=$cur_path/../config/1p_$ASCEND_DEVICE_ID.json
+#export RANK_ID=0
 
 wait
 
