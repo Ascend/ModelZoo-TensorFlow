2fc150a2145710a7d6b4769c27059fa0473b8025
赵鹏程 <3233928705@qq.com>
2022-04-21 11:15:00 +0000

    update run_estimator_tfrecord_classification.py. update

--- TensorFlow/built-in/recommendation/DeepCTR_Series_for_TensorFlow/examples/run_estimator_tfrecord_classification.py
+++ TensorFlow/built-in/recommendation/DeepCTR_Series_for_TensorFlow/examples/run_estimator_tfrecord_classification.py
@@ -29,13 +29,45 @@
 #
 from npu_bridge.npu_init import *
 import tensorflow as tf
-
+from tensorflow import keras
 from tensorflow.python.ops.parsing_ops import FixedLenFeature
 from deepctr.estimator import DeepFMEstimator
 from deepctr.estimator.inputs import input_fn_tfrecord
+import argparse
+import os
+def main():
 
-if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', default="./",
+                        help='data path for train')
+    parser.add_argument('--precision_mode', default='allow_fp32_to_fp16',
+                        help='allow_fp32_to_fp16/force_fp16/ '
+                             'must_keep_origin_dtype/allow_mix_precision.')
+    parser.add_argument('--profiling', default=False,
+                        help='if or not profiling for performance debug, default is False')
+    parser.add_argument('--profiling_dump_path', default="/home/data",
+                        help='the path to save profiling data')
+    args = parser.parse_args()
 
+    sess_config = tf.ConfigProto()
+    custom_op = sess_config.graph_options.rewrite_options.custom_optimizers.add()
+    sess_config.graph_options.rewrite_options.remapping = RewriterConfig.OFF
+    sess_config.graph_options.rewrite_options.memory_optimization = RewriterConfig.OFF
+    custom_op.name = "NpuOptimizer"
+    custom_op.parameter_map["precision_mode"].s = tf.compat.as_bytes(args.precision_mode)
+
+    if args.profiling:
+        custom_op.parameter_map["profiling_mode"].b = True
+        custom_op.parameter_map["profiling_options"].s = tf.compat.as_bytes(
+            '{"output":"' + args.profiling_dump_path + '", \
+                          "training_trace":"on", \
+                          "task_trace":"on", \
+                          "aicpu":"on", \
+                          "aic_metrics":"PipeUtilization",\
+                          "fp_point":"concatenate_1/concat", \
+                          "bp_point":"training/Adam/gradients/gradients/AddN_38"}')
+
+    npu_keras_sess = set_keras_session_npu_config(config=sess_config)
     # 1.generate feature_column for linear part and dnn part
 
     sparse_features = ['C' + str(i) for i in range(1, 27)]
@@ -72,4 +104,9 @@ if __name__ == "__main__":
     eval_result = model.evaluate(test_model_input)
 
     print(eval_result)
+    close_session(npu_keras_sess)
+
+if __name__ == "__main__":
+    main()
+
 
