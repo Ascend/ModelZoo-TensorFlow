f60911de50cdf4c1c1e1843a2c0989e5273af702
hxxhl88 <736544296@qq.com>
2022-04-24 16:28:39 +0800

    modify for param

--- TensorFlow/built-in/nlp/BertNV_Series_for_TensorFlow/src/run_squad.py
+++ TensorFlow/built-in/nlp/BertNV_Series_for_TensorFlow/src/run_squad.py
@@ -184,6 +184,7 @@ def extract_run_squad_flags():
   flags.DEFINE_bool('npu_bert_debug', False, 'If True, dropout and shuffle is disabled.')
   flags.DEFINE_integer('init_loss_scale_value', 2 ** 32, 'Initial loss scale value for loss scale optimizer')
   flags.DEFINE_integer("iterations_per_loop", 1, "How many steps to make in each estimator call.")
+  flags.DEFINE_bool("use_fp16_cls", False, "Whether to use fp16 in cls and pooler.")
   flags.DEFINE_bool('npu_bert_fused_gelu', True, 'Whether to use npu defined gelu op')
   flags.DEFINE_integer("npu_bert_loss_scale", 0,
                        "Whether to use loss scale, -1 is disable, 0 is dynamic loss scale, >=1 is static loss scale")
