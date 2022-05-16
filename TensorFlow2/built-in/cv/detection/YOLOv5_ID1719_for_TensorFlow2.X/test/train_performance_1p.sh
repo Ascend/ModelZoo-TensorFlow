05a02668cf192950e0c54c61d46ddd168e2833c7
王朵 <1556565970@qq.com>
2022-05-16 03:39:26 +0000

    修改first_step取值方式

--- TensorFlow2/built-in/cv/detection/YOLOv5_ID1719_for_TensorFlow2.X/test/train_performance_1p.sh
+++ TensorFlow2/built-in/cv/detection/YOLOv5_ID1719_for_TensorFlow2.X/test/train_performance_1p.sh
@@ -158,7 +158,7 @@ e2e_time=$(( $end_time - $start_time ))
 echo "------------------ Final result ------------------"
 #输出性能FPS。需要模型审视修改
 epoch_duration=`grep epoch_duration $cur_path/output/0/train_0.log | awk '{print $2}'`
-first_step=`grep duration: $cur_path/output/0/train_0.log |head -1| awk 'END{print $17}'`
+first_step=`grep duration: $cur_path/output/0/train_0.log |head -1| awk -F "duration:" '{print $2}'`
 FPS=`awk 'BEGIN{printf "%.2f\n",('$perf'+'$train_worker_num'-2)/('$epoch_duration'-'$first_step')*'$batch_size'*1}'`
 echo "Final Performance imgs/sec : $FPS"
 
