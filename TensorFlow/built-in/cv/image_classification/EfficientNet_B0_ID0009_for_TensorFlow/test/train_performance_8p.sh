26ec747f543ebd5563bcb83d61e30d3b48b17792
wangwei <641413440@qq.com>
2022-05-24 08:38:13 +0000

    update /EfficientNet_B0_ID0009_for_TensorFlow/test/train_performance_8p.sh.

--- TensorFlow/built-in/cv/image_classification/EfficientNet_B0_ID0009_for_TensorFlow/test/train_performance_8p.sh
+++ TensorFlow/built-in/cv/image_classification/EfficientNet_B0_ID0009_for_TensorFlow/test/train_performance_8p.sh
@@ -99,7 +99,7 @@ if [[ $data_path == "" ]];then
 fi
 
 #修改参数
-sed -i "695s|./efficientnet/ReduceMeanD.json|${cur_path}/../ReduceMeanD.json|g" $cur_path/../efficientnet/main_npu.py
+sed -i "695s|./efficientnet/ReduceMeanD.json|${cur_path}/../efficientnet/ReduceMeanD.json|g" $cur_path/../efficientnet/main_npu.py
 
 #autotune时，先开启autotune执行单P训练，不需要修改
 if [[ $autotune == True ]]; then
@@ -159,7 +159,7 @@ end_time=$(date +%s)
 e2e_time=$(( $end_time - $start_time ))
 
 #恢复参数
-sed -i "695s|${cur_path}/../ReduceMeanD.json|./efficientnet/ReduceMeanD.json|g" $cur_path/../efficientnet/main_npu.py
+sed -i "695s|${cur_path}/../efficientnet/ReduceMeanD.json|./efficientnet/ReduceMeanD.json|g" $cur_path/../efficientnet/main_npu.py
 
 #结果打印，不需要修改
 echo "------------------ Final result ------------------"
