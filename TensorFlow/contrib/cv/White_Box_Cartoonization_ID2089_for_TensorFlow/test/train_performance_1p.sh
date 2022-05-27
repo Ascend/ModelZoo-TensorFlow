ab27419f51ccb96e9ef8810d2326283be78eb020
张洋洋 <10527367+yang-yang-zhang123456@user.noreply.gitee.com>
2022-05-27 07:48:01 +0000

    /contrib/cv/White_Box_Cartoonization_ID2089_for_TensorFlow/test/train_performance_1p.sh.

--- TensorFlow/contrib/cv/White_Box_Cartoonization_ID2089_for_TensorFlow/test/train_performance_1p.sh
+++ TensorFlow/contrib/cv/White_Box_Cartoonization_ID2089_for_TensorFlow/test/train_performance_1p.sh
@@ -22,9 +22,9 @@ export JOB_ID=10087
 data_path=""
 output_path=""
 
-pip3 install scikit-image==0.14.5
-pip3 install numpy==1.19.2
-pip3 install pytorch-fid
+#pip3 install scikit-image==0.14.5
+#pip3 install numpy==1.19.2
+#pip3 install pytorch-fid
 
 # 帮助信息，不需要修改
 if [[ $1 == --help || $1 == -h ]];then
@@ -113,7 +113,7 @@ start_time=$(date +%s)
 # 您的训练数据集在${data_path}路径下，请直接使用这个变量获取
 # 您的训练输出目录在${output_path}路径下，请直接使用这个变量获取
 # 您的其他基础参数，可以自定义增加，但是batch_size请保留，并且设置正确的值
-train_steps=1000
+train_steps=100
 batch_size=16
 
 if [ x"${modelarts_flag}" != x ];
