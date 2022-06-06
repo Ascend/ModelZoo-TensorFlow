d5d1263dac72c9d7a3927d68524d4209aa9b0f7a
wangwei <641413440@qq.com>
2022-06-06 12:22:20 +0000

    update /ResNet50_ID0058_for_TensorFlow/test/train_full_8p.sh.

--- TensorFlow/built-in/cv/image_classification/ResNet50_ID0058_for_TensorFlow/test/train_full_8p.sh
+++ TensorFlow/built-in/cv/image_classification/ResNet50_ID0058_for_TensorFlow/test/train_full_8p.sh
@@ -127,7 +127,6 @@ do
     fi
 
     #执行训练脚本，需要模型审视修改
-	cd ${cur_path}/../src/mains
     corenum=`cat /proc/cpuinfo |grep 'processor' |wc -l`
     #let a=RANK_ID*${corenum}/8
     #let b=RANK_ID+1
@@ -136,7 +135,7 @@ do
     #    bind_core="taskset -c $a-$c"
     #fi
     #--max_train_steps=$max_train_steps \
-	python3.7 res50.py \
+	python3.7 ${cur_path}/../src/mains/res50.py \
 	    --config_file=$config_file \
 	    --iterations_per_loop=$iterations_per_loop \
 	    --debug=$debug \
