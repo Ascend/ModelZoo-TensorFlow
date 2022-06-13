ff3d9b2e26506c952e94eaa521f845dd3192204f
hqy <2609131663@qq.com>
2022-06-13 17:50:44 +0800

    训练精度达标，提交精度验收

---
+++ TensorFlow/contrib/cv/PCN_ID0997_for_TensorFlow/models/folding.py
@@ -0,0 +1,72 @@
+'''
+MIT License
+
+Copyright (c) 2018 Wentao Yuan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+from npu_bridge.npu_init import *
+import numpy as np
+import tensorflow as tf
+from tf_util import mlp_conv, chamfer, add_train_summary, add_valid_summary
+
+
+class Model:
+    def __init__(self, inputs, gt, alpha):
+        self.grid_size = 128
+        self.grid_scale = 0.5
+        self.num_output_points = 16384
+        self.features = self.create_encoder(inputs)
+        fold1, fold2 = self.create_decoder(self.features)
+        self.outputs = fold2
+        self.loss, self.update = self.create_loss(self.outputs, gt)
+        self.visualize_ops = [inputs[0], fold1[0], fold2[0], gt[0]]
+        self.visualize_titles = ['input', '1st folding', '2nd folding', 'ground truth']
+
+    def create_encoder(self, inputs):
+        with tf.variable_scope('encoder_0', reuse=tf.AUTO_REUSE):
+            features = mlp_conv(inputs, [128, 256])
+            features_global = tf.reduce_max(features, axis=1, keep_dims=True, name='maxpool_0')
+            features = tf.concat([features, tf.tile(features_global, [1, tf.shape(inputs)[1], 1])], axis=2)
+        with tf.variable_scope('encoder_1', reuse=tf.AUTO_REUSE):
+            features = mlp_conv(features, [512, 1024])
+            features = tf.reduce_max(features, axis=1, name='maxpool_1')
+        return features
+
+    def create_decoder(self, features):
+        with tf.variable_scope('decoder', reuse=tf.AUTO_REUSE):
+            x = tf.linspace(-self.grid_scale, self.grid_scale, self.grid_size)
+            y = tf.linspace(-self.grid_scale, self.grid_scale, self.grid_size)
+            grid = tf.meshgrid(x, y)
+            print("folding_grid:",grid)
+            grid = tf.reshape(tf.stack(grid, axis=2), [-1, 2])
+            grid = tf.tile(tf.expand_dims(grid, 0), [features.shape[0], 1, 1])
+            features = tf.tile(tf.expand_dims(features, 1), [1, self.num_output_points, 1])
+            with tf.variable_scope('folding_1'):
+                fold1 = mlp_conv(tf.concat([features, grid], axis=2), [512, 512, 3])
+            with tf.variable_scope('folding_2'):
+                fold2 = mlp_conv(tf.concat([features, fold1], axis=2), [512, 512, 3])
+        return fold1, fold2
+
+    def create_loss(self, outputs, gt):
+        loss = chamfer(outputs, gt)
+        add_train_summary('train/loss', loss)
+        update_loss = add_valid_summary('valid/loss', loss)
+        return loss, update_loss
+
---
+++ TensorFlow/contrib/cv/PCN_ID0997_for_TensorFlow/models/pcn_cd.py
@@ -0,0 +1,95 @@
+'''
+MIT License
+
+Copyright (c) 2018 Wentao Yuan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+from npu_bridge.npu_init import *
+import numpy as np
+import tensorflow as tf
+import tensorflow.compat.v1 as tf
+from tf_util import *
+
+
+class Model:
+    def __init__(self, inputs, npts, gt, alpha, batch_size):
+        self.num_coarse = 1024
+        self.grid_size = 4
+        self.grid_scale = 0.05
+        self.batch_size = batch_size
+        self.num_fine = self.grid_size ** 2 * self.num_coarse
+        self.features = self.create_encoder(inputs, npts)
+        self.coarse, self.fine = self.create_decoder(self.features)
+        self.loss, self.update = self.create_loss(self.coarse, self.fine, gt, alpha)
+        self.outputs = self.fine
+        self.visualize_ops = [tf.split(inputs[0], npts, axis=0), self.coarse, self.fine, gt]
+        self.visualize_titles = ['input', 'coarse output', 'fine output', 'ground truth']
+
+    def create_encoder(self, inputs, npts):
+        with tf.variable_scope('encoder_0', reuse=tf.AUTO_REUSE):
+            features = mlp_conv(inputs, [128, 256])
+            features_global = point_unpool(point_maxpool(features, npts, keepdims=True), npts)
+            features = tf.concat([features, features_global], axis=2)
+        with tf.variable_scope('encoder_1', reuse=tf.AUTO_REUSE):
+            features = mlp_conv(features, [512, 1024])
+            features = point_maxpool(features, npts)
+        return features
+
+    def create_decoder(self, features):
+        with tf.variable_scope('decoder', reuse=tf.AUTO_REUSE):
+            coarse = mlp(features, [1024, 1024, self.num_coarse * 3])
+            #print(coarse.shape)
+            coarse = tf.reshape(coarse, [coarse.shape[0], self.num_coarse, 3])
+
+        with tf.variable_scope('folding', reuse=tf.AUTO_REUSE):
+            grid = tf.meshgrid(tf.linspace(-0.05, 0.05, self.grid_size), tf.linspace(-0.05, 0.05, self.grid_size))
+            print("grid:",grid)
+            grid = tf.expand_dims(tf.reshape(tf.stack(grid, axis=2), [-1, 2]), 0)
+            grid_feat = tf.tile(grid, [features.shape[0], self.num_coarse, 1])
+
+            point_feat = tf.tile(tf.expand_dims(coarse, 2), [1, 1, self.grid_size ** 2, 1])
+            #print("point_feature:",point_feat.shape,' ',self.num_fine)
+            point_feat = tf.reshape(point_feat, [point_feat.shape[0], self.num_fine, 3])
+
+            global_feat = tf.tile(tf.expand_dims(features, 1), [1, self.num_fine, 1])
+
+            feat = tf.concat([grid_feat, point_feat, global_feat], axis=2)
+
+            center = tf.tile(tf.expand_dims(coarse, 2), [1, 1, self.grid_size ** 2, 1])
+            center = tf.reshape(center, [center.shape[0], self.num_fine, 3])
+
+            fine = mlp_conv(feat, [512, 512, 3]) + center
+        return coarse, fine
+
+    def create_loss(self, coarse, fine, gt, alpha):
+        loss_coarse = chamfer(coarse, gt)
+        add_train_summary('train/coarse_loss', loss_coarse)
+        update_coarse = add_valid_summary('valid/coarse_loss', loss_coarse)
+
+        loss_fine = chamfer(fine, gt)
+        add_train_summary('train/fine_loss', loss_fine)
+        update_fine = add_valid_summary('valid/fine_loss', loss_fine)
+
+        loss = loss_coarse + alpha * loss_fine
+        add_train_summary('train/loss', loss)
+        update_loss = add_valid_summary('valid/loss', loss)
+
+        return loss, [update_coarse, update_fine, update_loss]
+
