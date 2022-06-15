1a8653fee8d8ca6dbff4387e091a37d71c6b9f46
张文轩 <10359565+zhang-wenxuan09@user.noreply.gitee.com>
2022-06-15 02:31:11 +0000

    text_extraction_with_bert_ID2564_for_TensorFlow2.X移仓

---
+++ TensorFlow2/built-in/keras_sample/text_extraction_with_bert_ID2564_for_TensorFlow2.X/test/train_full_1p.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+#当前路径,不需要修改
+cur_path=`pwd`
+#export ASCEND_SLOG_PRINT_TO_STDOUT=1
+#集合通信参数,不需要修改
+
+export RANK_SIZE=1
+export JOB_ID=10087
+RANK_ID_START=0
+
+# 数据集路径,保持为空,不需要修改
+data_path=""
+
+#基础参数，需要模型审视修改
+#网络名称，同目录名称
+Network="text_extraction_with_bert_ID2564_for_TensorFlow2.X"
+#训练epoch
+train_epochs=5
+#训练batch_size
+batch_size=8
+
+
+#参数配置
+data_path=""
+############维测参数##############
+#precision_mode="allow_fp32_to_fp16"
+precision_mode="allow_mix_precision"
+#维持参数，以下不需要修改
+over_dump=False
+if [[ $over_dump == True ]];then
+    over_dump_path=${cur_path}/overflow_dump
+    mkdir -p ${over_dump_path}
+fi
+auto_tune=False
+data_dump_flag=False
+data_dump_step="10"
+profiling=False
+use_mixlist=False
+mixlist_file=${cur_path}/../configs/ops_info.json
+fusion_off_flag=False
+fusion_off_file=${cur_path}/../configs/fusion_switch.cfg
+############维测参数##############
+############维测参数##############
+for para in $*
+do
+    if [[ $para == --precision_mode* ]];then
+        precision_mode=`echo ${para#*=}`
+    elif [[ $para == --over_dump* ]];then
+        over_dump=`echo ${para#*=}`
+        over_dump_path=${cur_path}/output/overflow_dump
+        mkdir -p ${over_dump_path}
+    elif [[ $para == --data_dump_flag* ]];then
+        data_dump_flag=`echo ${para#*=}`
+        data_dump_path=${cur_path}/output/data_dump
+        mkdir -p ${data_dump_path}
+    elif [[ $para == --data_dump_step* ]];then
+        data_dump_step=`echo ${para#*=}`
+    elif [[ $para == --profiling* ]];then
+        profiling=`echo ${para#*=}`
+        profiling_dump_path=${cur_path}/output/profiling
+        mkdir -p ${profiling_dump_path}
+    elif [[ $para == --data_path* ]];then
+        data_path=`echo ${para#*=}`
+    elif [[ $para == --use_mixlist* ]];then
+        use_mixlist=`echo ${para#*=}`
+    elif [[ $para == --mixlist_file* ]];then
+        mixlist_file=`echo ${para#*=}`
+    elif [[ $para == --fusion_off_flag* ]];then
+        fusion_off_flag=`echo ${para#*=}`
+    elif [[ $para == --fusion_off_file* ]];then
+        fusion_off_file=`echo ${para#*=}`
+    elif [[ $para == --auto_tune* ]];then
+        auto_tune=`echo ${para#*=}`
+    fi
+done
+############维测参数##############
+
+#校验是否传入data_path,不需要修改
+if [[ $data_path == "" ]];then
+    echo "[Error] para \"data_path\" must be confing"
+    exit 1
+fi
+
+#训练开始时间，不需要修改
+start_time=$(date +%s)
+
+#进入训练脚本目录，需要模型审视修改
+cd $cur_path/../
+
+for((RANK_ID=$RANK_ID_START;RANK_ID<$((RANK_SIZE+RANK_ID_START));RANK_ID++));
+do
+    #设置环境变量，不需要修改
+    echo "Device ID: $ASCEND_DEVICE_ID"
+    export RANK_ID=$RANK_ID
+    
+    
+    
+    #创建DeviceID输出目录，不需要修改
+    if [ -d ${cur_path}/output/${ASCEND_DEVICE_ID} ];then
+        rm -rf ${cur_path}/output/${ASCEND_DEVICE_ID}
+        mkdir -p ${cur_path}/output/$ASCEND_DEVICE_ID/ckpt
+    else
+        mkdir -p ${cur_path}/output/$ASCEND_DEVICE_ID/ckpt
+    fi
+
+    #执行训练脚本，以下传参不需要修改，其他需要模型审视修改
+    nohup python3 text_extraction_with_bert.py  --data_path=$data_path \
+                                        --epochs=${train_epochs} \
+                                        --batch_size=${batch_size} \
+                                        --precision_mode=${precision_mode} \
+                                        --over_dump=${over_dump} \
+                                        --over_dump_path=${over_dump_path} \
+                                        --data_dump_flag=${data_dump_flag} \
+                                        --data_dump_step=${data_dump_step} \
+                                        --data_dump_path=${data_dump_path} \
+                                        --profiling=${profiling} \
+                                        --use_mixlist=${use_mixlist} \
+                                        --fusion_off_flag=${fusion_off_flag} \
+                                        --mixlist_file=${mixlist_file} \
+                                        --auto_tune=${auto_tune} \
+                                        --fusion_off_file=${fusion_off_file} \
+                                        --profiling_dump_path=${profiling_dump_path} \
+                                        --part_steps=0 > ${cur_path}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log 2>&1 &
+                                    
+                                        
+done 
+wait
+
+#conda deactivate
+#训练结束时间，不需要修改
+end_time=$(date +%s)
+e2e_time=$(( $end_time - $start_time ))
+
+Step=`grep loss: $cur_path/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log | tail -1 | awk '{print $1}' | awk -F "/" '{print $1}' |awk '{sum+=$1} END {print sum/NR}'`
+Time=`grep loss: $cur_path/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log | tail -1 | awk '{print $3}' | tr -d s | awk '{sum+=$1} END {print sum/NR}'`
+TrainingTime=`awk 'BEGIN{printf "%.6f\n",'${Time}'*1000/'${Step}'}'`
+FPS=`awk 'BEGIN{printf "%.2f\n",'${batch_size}'*'${Step}'/'${Time}'}'`
+wait
+
+#输出训练精度,需要模型审视修改
+train_accuracy=`grep "match" $cur_path/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk -F "score=" '{print $2}'|tail -1`
+#打印，不需要修改
+echo "Final Train Accuracy : ${train_accuracy}"
+echo "E2E Training Duration sec : $e2e_time"
+
+#稳定性精度看护结果汇总
+#训练用例信息，不需要修改
+BatchSize=${batch_size}
+DeviceType=`uname -m`
+CaseName=${Network}_bs${BatchSize}_${RANK_SIZE}'p'_'acc'
+
+##获取性能数据
+#吞吐量，不需要修改
+ActualFPS=${FPS}
+
+
+#从train_$ASCEND_DEVICE_ID.log提取Loss到train_${CaseName}_loss.txt中，需要根据模型审视
+grep "loss:" $cur_path/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log|awk  '{print $6}' >> $cur_path/output/$ASCEND_DEVICE_ID/train_${CaseName}_loss.txt
+
+#最后一个迭代loss值，不需要修改
+ActualLoss=`awk 'END {print}' $cur_path/output/$ASCEND_DEVICE_ID/train_${CaseName}_loss.txt`
+
+#关键信息打印到${CaseName}.log中，不需要修改
+echo "Network = ${Network}" > $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "RankSize = ${RANK_SIZE}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "BatchSize = ${BatchSize}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "DeviceType = ${DeviceType}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "CaseName = ${CaseName}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "ActualFPS = ${ActualFPS}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "TrainingTime = ${TrainingTime}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "TrainAccuracy = ${train_accuracy}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "ActualLoss = ${ActualLoss}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "E2ETrainingTime = ${e2e_time}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
