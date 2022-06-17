d59e4e15330edd19d629810f53cc4e5c4e1a9496
张烜 <xuanzhang@sjtu.edu.cn>
2022-06-17 19:28:58 +0800

    update

--- ACL_TensorFlow/contrib/cv/RANDLA-NET_ID0850_for_ACL/eval_acc.py
+++ ACL_TensorFlow/contrib/cv/RANDLA-NET_ID0850_for_ACL/eval_acc.py
@@ -1,3 +1,34 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Qingyong Hu (huqingyong15@outlook.com) 15/11/2019
+
+
 import numpy as np
 from sklearn.metrics import confusion_matrix
 # import open3d
@@ -100,9 +131,9 @@ def IoU_from_confusions(confusions):
 #         return Y_semins
 
 
-labels_file = "D://huawei//离线推理//bin_out_final//labels.bin"
-stacked_probs_file = "D://huawei//离线推理//bin_out_final//randlanet_output_0.bin"
-pc_xyz_file = "D://huawei//离线推理//bin_out_final//xyz_0.bin"
+labels_file = "/root/bin_out/labels.bin"
+stacked_probs_file = "/root/bin_out/randlanet_output_0.bin"
+pc_xyz_file = "/root/bin_out/xyz_0.bin"
 num_classes = 13
 batch_size = 3
 point_num = 40960
