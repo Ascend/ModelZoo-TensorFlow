538333e84dd137459ec421eb50b6ba4aaf86b687
xiayankang <xiayankang@outlook.com>
2022-06-27 01:20:19 +0000

    update TensorFlow/contrib/cv/MVDSCN_ID1272_for_TensorFlow/model/__init__.py.

--- TensorFlow/contrib/cv/MVDSCN_ID1272_for_TensorFlow/model/__init__.py
+++ TensorFlow/contrib/cv/MVDSCN_ID1272_for_TensorFlow/model/__init__.py
@@ -1,7 +1,7 @@
 from npu_bridge.npu_init import *
 import os
-os.system('pip install munkres')
-os.system('pip install sklearn')
-os.system('pip install scipy')
+# os.system('pip install munkres')
+# os.system('pip install sklearn')
+# os.system('pip install scipy')
 
 
