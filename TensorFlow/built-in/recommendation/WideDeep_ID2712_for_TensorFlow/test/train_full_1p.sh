327bee3763a846b1cf8ba98d0f7297a3acfb336a
majun121 <867479212@qq.com>
2022-07-11 07:07:05 +0000

    update WideDeep_ID2712_for_TensorFlow/test/train_full_1p.sh.

--- TensorFlow/built-in/recommendation/WideDeep_ID2712_for_TensorFlow/test/train_full_1p.sh
+++ TensorFlow/built-in/recommendation/WideDeep_ID2712_for_TensorFlow/test/train_full_1p.sh
@@ -116,7 +116,9 @@ mv -f configs/config.py.bak configs/config.py
 echo "------------------ Final result ------------------"
 #输出性能FPS，需要模型审视修改
 
-FPS=`grep 'fps :'  $cur_path/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log| awk -F' ' '{print $25}' | tail -n 1`
+#FPS=`grep 'fps :'  $cur_path/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log| awk -F' ' '{print $25}' | tail -n 1`
+time=`grep -rn 'epoch 2 total time =:'  $cur_path/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log| awk -F '=' '{print $2}'`
+FPS=`awk 'BEGIN{printf "%.2f\n",'100'*'${batch_size}'/'${time}'}'`
 
 #打印，不需要修改
 echo "Final Performance images/sec : $FPS"
