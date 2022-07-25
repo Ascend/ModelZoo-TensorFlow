8a70dd57ae921134ffbc38dfb5e72196a2b44ea5
张文轩 <10359565+zhang-wenxuan09@user.noreply.gitee.com>
2022-07-25 11:44:13 +0000

    update TensorFlow2/built-in/keras_sample/cv/convmixer_ID2501_for_TensorFlow2.X/train.py.

--- TensorFlow2/built-in/keras_sample/cv/convmixer_ID2501_for_TensorFlow2.X/train.py
+++ TensorFlow2/built-in/keras_sample/cv/convmixer_ID2501_for_TensorFlow2.X/train.py
@@ -297,7 +297,7 @@ if not hasattr(npu_device.ops, 'gelu') and "FastGelu" not in grad_registry_list:
 
 def activation_block(x):
     #x = layers.Activation("gelu")(x)
-     if not hasattr(npu_device.ops, 'gelu'):
+    if not hasattr(npu_device.ops, 'gelu'):
       x = npu_device.gen_npu_ops.fast_gelu(x)
     else:
       x = getattr(npu_device.ops, 'gelu')
