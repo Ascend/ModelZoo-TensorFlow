ade4f17718cd717ab15fb3c54253ffee63ca73c0
tongxinlong <2923652651@qq.com>
2022-08-02 05:50:46 +0000

    update Te

--- TensorFlow/built-in/cv/image_segmentation/UNet3D_ID0057_for_TensorFlow/test/train_performance_16np.sh
+++ TensorFlow/built-in/cv/image_segmentation/UNet3D_ID0057_for_TensorFlow/test/train_performance_16np.sh
@@ -57,10 +57,12 @@ do
         conf_path=`echo ${para#*=}`
     elif [[ $para == --devices_num* ]];then
 	    devices_num=`echo ${para#*=}`
+    elif [[ $para == --servers_num* ]];then
+        servers_num=`echo ${para#*=}`
     fi
 done
-one_node_ip=`find $conf_path -name "server_*0.info"|awk -F "server_" '{print $2}'|awk -F "_" '{print $1}'`
-linux_num=`find $conf_path -name "server_*.info" |wc -l`
+
+linux_num=$servers_num
 
 #export ASCEND_SLOG_PRINT_TO_STDOUT=1
 export RANK_SIZE=`awk 'BEGIN{printf "%.0f\n",'${devices_num}'*'${linux_num}'}'`
