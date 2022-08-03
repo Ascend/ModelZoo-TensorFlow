d43e216beaf929fe2e0ee5083f71fb929efa0e8d
张文轩 <10359565+zhang-wenxuan09@user.noreply.gitee.com>
2022-08-03 09:30:41 +0000

    update MNIST_ID2481_for_TensorFlow2.X/test/train_performance_1p_dynamic_eval.sh.

--- TensorFlow2/built-in/cv/image_classification/MNIST_ID2481_for_TensorFlow2.X/test/train_performance_1p_dynamic_eval.sh
+++ TensorFlow2/built-in/cv/image_classification/MNIST_ID2481_for_TensorFlow2.X/test/train_performance_1p_dynamic_eval.sh
@@ -1,64 +1,123 @@
 #!/bin/bash
 
 cur_path=`pwd`/../
-#失败用例打屏
-export ASCEND_SLOG_PRINT_TO_STDOUT=1
+
+export RANK_SIZE=8
+export JOB_ID=10087
+export RANK_TABLE_FILE=$cur_path/test/${RANK_SIZE}p.json
+export ASCEND_GLOBAL_LOG_LEVEL_ETP_ETP_ETP=3
 
 #基础参数，需要模型审视修改
 #Batch Size
 batch_size=1024
 #网络名称，同目录名称
 Network="MNIST_ID2481_for_TensorFlow2.X"
-#Device数量，单卡默认为1
-RankSize=1
 #训练epoch，可选
 train_epochs=10
-#训练step
-#train_steps=5
-#学习率
-#learning_rate=1e-4
-#ckpt_path=""
 #参数配置
-data_path=""
-#work_dir="$cur_path/estimator_working_dir"
-#export_path="$cur_path/outputs/models/000001-first_generation"
+data_path="/npu/traindata/ID2481_CarPeting_TF2.X_MNIST"
 
-if [[ $1 == --help || $1 == --h ]];then
-   echo "usage:./train_performance_1p.sh $data_path --work_dir="$cur_path/estimator_working_dir" --export_path="$cur_path/outputs/models/000001-first_generation""
-   exit 1
+############维测参数##############
+precision_mode="allow_mix_precision"
+#维持参数，以下不需要修改
+over_dump=False
+if [[ $over_dump == True ]];then
+    over_dump_path=$cur_path/test/overflow_dump #此处cur_path为代码根目录
+    mkdir -p ${over_dump_path}
 fi
+data_dump_flag=False
+data_dump_step="10"
+profiling=False
+use_mixlist=False
+mixlist_file="ops_info.json"
+fusion_off_flag=False
+fusion_off_file="fusion_switch.cfg"
+############维测参数##############
 
+############维测参数##############
 for para in $*
 do
-   if [[ $para == --data_path* ]];then
-      data_path=`echo ${para#*=}`
-   fi
-   if [[ $para == --ckpt_path* ]];then
-      ckpt_path=`echo ${para#*=}`
-   fi
+    if [[ $para == --precision_mode* ]];then
+        precision_mode=`echo ${para#*=}`
+    elif [[ $para == --over_dump* ]];then
+        over_dump=`echo ${para#*=}`
+        over_dump_path=${cur_path}/output/overflow_dump
+        mkdir -p ${over_dump_path}
+    elif [[ $para == --data_dump_flag* ]];then
+        data_dump_flag=`echo ${para#*=}`
+        data_dump_path=${cur_path}/output/data_dump
+        mkdir -p ${data_dump_path}
+    elif [[ $para == --data_dump_step* ]];then
+        data_dump_step=`echo ${para#*=}`
+    elif [[ $para == --profiling* ]];then
+        profiling=`echo ${para#*=}`
+        profiling_dump_path=${cur_path}/output/profiling
+        mkdir -p ${profiling_dump_path}
+    elif [[ $para == --data_path* ]];then
+        data_path=`echo ${para#*=}`
+    elif [[ $para == --use_mixlist* ]];then
+        use_mixlist=`echo ${para#*=}`
+    elif [[ $para == --mixlist_file* ]];then
+        mixlist_file=`echo ${para#*=}`
+    elif [[ $para == --fusion_off_flag* ]];then
+        fusion_off_flag=`echo ${para#*=}`
+    elif [[ $para == --fusion_off_file* ]];then
+        fusion_off_file=`echo ${para#*=}`
+    fi
 done
+############维测参数##############
 
 if [[ $data_path  == "" ]];then
    echo "[Error] para \"data_path\" must be config"
    exit 1
 fi
 
-cd $cur_path
 
-##############执行训练##########
-if [ -d $cur_path/test/output ];then
-   rm -rf $cur_path/test/output/*
-   mkdir -p $cur_path/test/output/$ASCEND_DEVICE_ID
-else
-   mkdir -p $cur_path/test/output/$ASCEND_DEVICE_ID
-fi
-wait
-
-#sed -i 's/".."/$data_path/g' ./model/config.1_FRLR.unetxst.yml
-#sed -i 's/"steps_per_epoch=n_batches_train"/"steps_per_epoch=5"/g' ./model/train.py
 start=$(date +%s)
-nohup python3 mnist_main.py --model_dir=./ckpt --train_epochs=10 --distribution_strategy=one_device --num_gpus=1 --download=False --eval_static=False --data_dir=$data_path >$cur_path/test/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log 2>&1 &
+##############执行训练##########
+RANK_ID_START=0
+for((RANK_ID=$RANK_ID_START;RANK_ID<$((RANK_SIZE+RANK_ID_START));RANK_ID++));
+do
+    cd $cur_path
+    #设置环境变量，不需要修改
+    export RANK_ID=$RANK_ID
+    export ASCEND_DEVICE_ID=$RANK_ID
+    echo "Device ID: $ASCEND_DEVICE_ID"
+
+    #创建DeviceID输出目录，不需要修改
+    if [ -d ${cur_path}/test/output/${ASCEND_DEVICE_ID} ];then
+        rm -rf ${cur_path}/test/output/${ASCEND_DEVICE_ID}
+        mkdir -p ${cur_path}/test/output/$ASCEND_DEVICE_ID
+    else
+        mkdir -p ${cur_path}/test/output/$ASCEND_DEVICE_ID
+    fi
+    cpucount=`lscpu | grep "CPU(s):" | head -n 1 | awk '{print $2}'`
+    cpustep=`expr $cpucount / 8`
+    python3 mnist_main.py --precision_mode=${precision_mode} \
+             --over_dump=${over_dump} \
+             --over_dump_path=${over_dump_path} \
+             --data_dump_flag=${data_dump_flag} \
+             --data_dump_step=${data_dump_step} \
+             --data_dump_path=${data_dump_path} \
+             --profiling=${profiling} \
+             --use_mixlist=${use_mixlist} \
+             --fusion_off_flag=${fusion_off_flag} \
+             --mixlist_file=${mixlist_file} \
+             --fusion_off_file=${fusion_off_file} \
+             --profiling_dump_path=${profiling_dump_path} \
+             --model_dir=./ckpt \
+             --train_epochs=10 \
+             --distribution_strategy=one_device \
+             --num_gpus=1 \
+             --download=False \
+             --eval_static=False \
+             --mul_rank_size=${RANK_SIZE} \
+             --mul_device_id=${RANK_ID} \
+             --data_dir=$data_path >$cur_path/test/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log 2>&1 &
+done
 wait
+
+##############结束训练##########
 end=$(date +%s)
 e2etime=$(( $end - $start ))
 
@@ -66,31 +125,47 @@ e2etime=$(( $end - $start ))
 #echo "Final Performance ms/step : $average_perf"
 echo "Final Training Duration sec : $e2etime"
 
-###下面字段用于冒烟看护
+#结果打印，不需要修改
+echo "------------------ Final result ------------------"
+
+#输出性能FPS，需要模型审视修改
+TrainingTime=`grep ,time: $cur_path/test/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log | awk '{print $4}' | awk -F ':' '{print $2}' | tail -n 1`
+wait
+FPS=`grep imgs/s $cur_path/test/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log | awk '{print '${RANK_SIZE}'*$2}'`
+#打印，不需要修改
+echo "Final Performance images/sec : $FPS"
+
+#输出训练精度,需要模型审视修改
+train_accuracy=`grep sparse_categorical_accuracy $cur_path/test/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log |awk 'END {print $NF}'`
+#打印，不需要修改
+echo "Final Train Accuracy : ${train_accuracy}"
+
+
+#精度看护结果汇总
+#训练用例信息，不需要修改
 BatchSize=${batch_size}
-#设备类型，自动获取
 DeviceType=`uname -m`
-#用例名称，自动获取
-CaseName=${Network}_bs${BatchSize}_${RankSize}'p'_'perf'
+CaseName=${Network}_bs${BatchSize}_${RANK_SIZE}'p'_dynamic_eval_'perf' 
 
-##获取错误信息
-#系统错误信息
-error_msg="Graph engine process graph failed: EZ3002: Optype \[Conv2DBackpropFilter\] of Ops kernel"
-#判断错误信息是否和历史状态一致，此处无需修改
-Status=`grep "${error_msg}" $cur_path/test/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log | wc -l`
-#失败阶段，枚举值图准备FAIL/图拆分FAIL/图优化FAIL/图编译FAIL/图执行FAIL/流程OK
-ModelStatus="图执行FAIL"
-#DTS单号或者issue链接
-DTS_Number="DTS2021090622224"
+##获取性能数据，不需要修改
+#吞吐量
+ActualFPS=${FPS}
+#单迭代训练时长
+TrainingTime=${TrainingTime}
 
-#关键信息打印到CaseName.log中，此处无需修改
+#从train_$ASCEND_DEVICE_ID.log提取Loss到train_${CaseName}_loss.txt中，需要根据模型审视
+grep ,loss: $cur_path/test/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log | awk '{print $3}' >> $cur_path/test/output/$ASCEND_DEVICE_ID/train_${CaseName}_loss.txt 
+#最后一个迭代loss值，不需要修改
+ActualLoss=`awk 'END {print $1}' $cur_path/test/output/$ASCEND_DEVICE_ID/train_${CaseName}_loss.txt`
+
+#关键信息打印到${CaseName}.log中，不需要修改
 echo "Network = ${Network}" > $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
-echo "RankSize = ${RankSize}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "RankSize = ${RANK_SIZE}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
 echo "BatchSize = ${BatchSize}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
 echo "DeviceType = ${DeviceType}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
 echo "CaseName = ${CaseName}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
-echo "ModelStatus = ${ModelStatus}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
-echo "DTS_Number = ${DTS_Number}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
-echo "Status = ${Status}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
-echo "error_msg = ${error_msg}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "ActualFPS = ${ActualFPS}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "TrainingTime = ${TrainingTime}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "TrainAccuracy = ${train_accuracy}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "ActualLoss = ${ActualLoss}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
 echo "E2ETrainingTime = ${e2etime}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
