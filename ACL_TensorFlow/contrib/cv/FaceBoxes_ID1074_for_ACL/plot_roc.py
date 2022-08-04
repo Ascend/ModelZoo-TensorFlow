09db63c41c54afa3fa985548b052a99fb784c06b
Bebad <740101715@qq.com>
2022-08-04 06:29:42 +0000

    评价指标roc绘制

---
+++ ACL_TensorFlow/contrib/cv/FaceBoxes_ID1074_for_ACL/plot_roc.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+rocfile='C:/Users/DELL/Desktop/huawei/FaceBoxes-GPU/DiscROC.txt'
+roc = pd.read_csv(rocfile, sep=' ', header=None)
+roc.columns = ['tpr', 'fp', 'threshold']
+def plot_roc():
+    _, axis = plt.subplots(nrows=1, ncols=1, figsize=(7, 4), dpi=120)
+    axis.plot(roc.fp, roc.tpr, c='r', linewidth=2.0);
+    axis.set_title('Discrete Score ROC')
+    axis.set_xlim([0, 2000.0])
+    axis.set_ylim([0.6, 1.0])
+    axis.set_xlabel('False Positives')
+    axis.set_ylabel('True Positive Rate');
+    plt.show()
+
+plot_roc()
