915adbb1adb4cccc690e17f4b59c9175e45b1257
CCCP <648976749@qq.com>
2022-08-11 01:09:01 +0800

    prsr_pr

--- TensorFlow/contrib/cv/prsr/PRSR_ID2111_for_TensorFlow/solver.py
+++ TensorFlow/contrib/cv/prsr/PRSR_ID2111_for_TensorFlow/solver.py
@@ -1,37 +1,24 @@
-# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ============================================================================
-# Copyright 2021 Huawei Technologies Co., Ltd
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 from npu_bridge.npu_init import *
 import tensorflow as tf
+
+# 引用precision_tool/tf_config.py
+import precision_tool.tf_config as npu_tf_config
+
+# #------------------ Dump数据采集 ------------------------
+import argparse
+# import precision_tool.tf_config as npu_tf_config
+# import moxing as mox
+import precision_tool.config as CONFIG
+# #------------------ Dump数据采集 ------------------------
+
+# #------------------ NPU 关闭融合规则 ----------------------
+# import precision_tool.tf_config as npu_tf_config
+# #------------------ NPU 关闭融合规则 ----------------------
+
 import numpy as np
 from ops import *
 from data import *
@@ -39,16 +26,6 @@ from net import *
 from utils import *
 import os
 import time
-
-# #------------------11.4 Dump数据采集------------------------
-# # 引用precision_tool/tf_config.py
-# import argparse
-# import precision_tool.tf_config as npu_tf_config
-# import moxing as mox
-# import precision_tool.config as CONFIG
-# #------------------11.4 Dump数据采集------------------------
-
-
 flags = tf.app.flags
 conf = flags.FLAGS
 
@@ -62,7 +39,7 @@ class Solver(object):
 			os.makedirs(self.train_dir)
 		if not os.path.exists(self.samples_dir):
 			os.makedirs(self.samples_dir)
-			# datasets params
+		# datasets params
 		self.num_epoch = conf.num_epoch
 		self.batch_size = conf.batch_size
 		# optimizer parameter
@@ -81,38 +58,48 @@ class Solver(object):
 		learning_rate = tf.train.exponential_decay(self.learning_rate, self.global_step,
 		                                           500000, 0.5, staircase = True)
 		optimizer = tf.train.RMSPropOptimizer(learning_rate, decay = 0.95, momentum = 0.9, epsilon = 1e-8)
+
+		# ------------------- NPU LossScale -------------------------
+		self.loss_scale_manager = ExponentialUpdateLossScaleManager(init_loss_scale = 2 ** 32, incr_every_n_steps = 1000,
+		                                                       decr_every_n_nan_or_inf = 2, incr_ratio = 2,
+		                                                       decr_ratio = 0.8)
+		optimizer = NPULossScaleOptimizer(optimizer, self.loss_scale_manager)
+		# ------------------- NPU LossScale -------------------------
+
 		self.train_op = optimizer.minimize(self.net.loss, global_step = self.global_step)
+
+
 
 	def train(self):
 		init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
 		summary_op = tf.summary.merge_all()
 		saver = tf.train.Saver()
-		# Create a session for running operations in the Graph.
-		# config = tf.ConfigProto(allow_soft_placement=True)
-		# config.gpu_options.allow_growth = True
 
-		# ----------------------------2021.9.11 NPU-------------------------------------
+		# # ----------------- 溢出检测 --------------------------------
+		# config = tf.ConfigProto()
+		# config = npu_tf_config.session_dump_config(config, action = 'overflow')
+		# # -------------------------------------------------------
+
+		#----------------- NPU --------------------------------
 		config = tf.ConfigProto()
-
-		# # ------------------11.4 Dump数据采集------------------------
-		# config = npu_tf_config.session_dump_config(config, action = 'dump')
-		# # ------------------11.4 Dump数据采集------------------------
-
 		custom_op = config.graph_options.rewrite_options.custom_optimizers.add()
 		custom_op.name = "NpuOptimizer"
 
-		# ----------------------2021.9.16 NPU-------------
-		custom_op.parameter_map["mix_compile_mode"].b = True
-		# ----------------------2021.9.16 NPU-------------
-
-		# # ----------------------2021.10.31 NPU-------------
-		# custom_op.parameter_map["use_off_line"].b = True
-		# custom_op.parameter_map["precision_mode"].s = tf.compat.as_bytes("allow_mix_precision")
-		# # ----------------------2021.10.31 NPU-------------
-
 		config.graph_options.rewrite_options.remapping = RewriterConfig.OFF  # 必须显式关闭remap
-		config.graph_options.rewrite_options.memory_optimization = RewriterConfig.OFF
-		# ----------------------------2021.9.11 NPU-------------------------------------
+		#-------------------------------------------------------
+
+		# ------------------ NPU 混合精度 ----------------------------
+		custom_op.parameter_map["precision_mode"].s = tf.compat.as_bytes("allow_mix_precision")
+		custom_op.parameter_map["modify_mixlist"].s = tf.compat.as_bytes("/home/ma-user/modelarts/user-job-dir/code/ops_info.json")
+		# ----------------------------------------------------------
+
+		# #---------------- NPU DUMP --------------------------
+		# config = npu_tf_config.session_dump_config(config, action = 'dump')
+		# #-----------------------------------------------------
+
+		# # ------------------ NPU 关闭融合规则 ----------------------
+		# config = npu_tf_config.session_dump_config(config, action = 'fusion_off')
+		# # -------------------------------------------------------
 
 		sess = tf.Session(config = config)
 
@@ -122,16 +109,22 @@ class Solver(object):
 		summary_writer = tf.summary.FileWriter(self.train_dir, sess.graph)
 		# Start input enqueue threads.
 		coord = tf.train.Coordinator()
-		threads = tf.train.start_queue_runners(sess = sess, coord = coord)
+		threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 		iters = 0
 		try:
-			while not (coord.should_stop() | iters == 380):
+			# while not (coord.should_stop() | iters == 380):
+			while not (coord.should_stop()):
 				# Run training steps or whatever
 				t1 = time.time()
 				_, loss = sess.run([self.train_op, self.net.loss], feed_dict = {self.net.train: True})
+				# _, loss, scale_value = sess.run(
+				# 		[self.train_op, self.net.loss, self.loss_scale_manager.get_loss_scale()],
+				# 		feed_dict = {self.net.train: True})
 				t2 = time.time()
-				print('step %d, loss = %.2f  %.1f examples/sec  %.3f sec/batch' % (
+				print('step %d, loss = %.2f (%.1f examples/sec; %.3f sec/batch)' % (
 				(iters, loss, self.batch_size / (t2 - t1), (t2 - t1))))
+				# print('step %d, loss = %.2f (%.1f examples/sec; %.3f sec/batch), scale:%d' % (
+				# 		(iters, loss, self.batch_size / (t2 - t1), (t2 - t1), scale_value)))
 				iters += 1
 				if iters % 10 == 0:
 					summary_str = sess.run(summary_op, feed_dict = {self.net.train: True})
@@ -139,16 +132,17 @@ class Solver(object):
 				if iters % 1000 == 0:
 					# self.sample(sess, mu=1.0, step=iters)
 					self.sample(sess, mu = 1.1, step = iters)
-					# self.sample(sess, mu=100, step=iters)
+				# self.sample(sess, mu=100, step=iters)
 				if iters % 10000 == 0:
 					checkpoint_path = os.path.join(self.train_dir, 'model.ckpt')
 					saver.save(sess, checkpoint_path, global_step = iters)
-				# if iters % 370 == 0:
+				if iters % 370 == 0:
+					continue
 				# 	parser = argparse.ArgumentParser()
 				# 	parser.add_argument("--train_url", type = str, default = "./output")
-				# 	config2, unparsed = parser.parse_known_args()
-					#mox.file.copy_parallel(CONFIG.ROOT_DIR, config2.train_url)
-					#mox.file.copy_parallel('/home/ma-user/modelarts/user-job-dir/code', config2.train_url)
+				# 	configs = parser.parse_args()
+				# 	mox.file.copy_parallel(CONFIG.ROOT_DIR, configs.train_url)
+				# # 	mox.file.copy_parallel('/home/homema-user/modelarts/user-job-dir/code', configs.train_url)
 		except tf.errors.OutOfRangeError:
 			checkpoint_path = os.path.join(self.train_dir, 'model.ckpt')
 			saver.save(sess, checkpoint_path)
