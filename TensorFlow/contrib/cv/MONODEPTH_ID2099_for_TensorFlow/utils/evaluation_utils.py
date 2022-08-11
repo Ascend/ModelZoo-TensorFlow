eb24dd6fc5e6ffca60caef8cf2b30785f768e731
Hide on Bush <424133169@qq.com>
2022-08-12 00:06:06 +0800

    fix error

--- TensorFlow/contrib/cv/MONODEPTH_ID2099_for_TensorFlow/utils/evaluation_utils.py
+++ TensorFlow/contrib/cv/MONODEPTH_ID2099_for_TensorFlow/utils/evaluation_utils.py
@@ -5,6 +5,9 @@ import cv2
 from collections import Counter
 import pickle
 
+from scipy.interpolate.interpnd import LinearNDInterpolator
+
+
 def compute_errors(gt, pred):
     thresh = np.maximum((gt / pred), (pred / gt))
     a1 = (thresh < 1.25   ).mean()
