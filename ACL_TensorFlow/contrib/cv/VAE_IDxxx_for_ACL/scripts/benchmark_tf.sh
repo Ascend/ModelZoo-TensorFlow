213c1d89f23e64d00f4e0c59e8681b06b36db0c1
zero167 <215051281@qq.com>
2022-08-19 03:08:06 +0000

    上传源码
    Signed-off-by: zero167 <215051281@qq.com>

---
+++ ACL_TensorFlow/contrib/cv/VAE_IDxxx_for_ACL/scripts/benchmark_tf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#set -x
+cur_dir=`pwd`
+benchmark_dir=$cur_dir/../Benchmark/out
+om_name=$cur_dir/../hh_om.om
+batchsize=1
+model_name=VAE
+output_dir='results'
+rm -rf $cur_dir/$output_dir/*
+
+#start offline inference
+$benchmark_dir/benchmark --om $om_name --dataDir $cur_dir/input_bins/ --modelType $model_name --outDir $cur_dir/$output_dir --batchSize $batchsize --imgType bin --useDvpp 0
+
