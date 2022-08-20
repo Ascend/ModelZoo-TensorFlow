12dc9d4cbda249ae1c98b8594bb5f049fbb2ba1e
alex <alex.cheng@huawei.com>
2022-08-20 07:31:57 +0000

    update ACL_TensorFlow/contrib/nlp/Roformer_for_ACL/scripts/generate_random_data.py.
    Signed-off-by: alex <alex.cheng@huawei.com>

--- ACL_TensorFlow/contrib/nlp/Roformer_for_ACL/scripts/generate_random_data.py
+++ ACL_TensorFlow/contrib/nlp/Roformer_for_ACL/scripts/generate_random_data.py
@@ -14,17 +14,29 @@
 
 import argparse
 import numpy as np
+import os
 
-def generate_random_data(file_path,image_num=32):
+def generate_random_data(file_path,image_num=32,batchsize=1):
+    if not os.path.exists(file_path):
+            os.makedirs(file_path)
+    sub_dir1 = os.path.join(file_path,'Input-Segment')
+    sub_dir2 = os.path.join(file_path,'Input-Token')
+    if not os.path.exists(sub_dir1):
+            os.makedirs(sub_dir1)
+    if not os.path.exists(sub_dir2):
+            os.makedirs(sub_dir2)
     for i in range(image_num):
-        input_data = np.random.randn(1,10000,4).astype(np.float32)
-        input_data.tofile(file_path+"/"+str(i.__str__().zfill(6))+".bin")
+        input_data1 = np.random.randn(batchsize,1024).astype(np.float32)
+        input_data2 = np.random.randn(batchsize,1024).astype(np.float32)
+        input_data1.tofile(os.path.join(sub_dir1,str(i.__str__().zfill(6))+".bin"))
+        input_data2.tofile(os.path.join(sub_dir2,str(i.__str__().zfill(6))+".bin"))
     print("num:%d random datas has been created under path:%s" %(image_num,file_path))
 
 if __name__ =="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--path",type=str,default="./")
     parser.add_argument("--nums", type=int, default=32)
+    parser.add_argument("--batchsize", type=int, default=1)
     args = parser.parse_args()
 
-    generate_random_data(args.path,args.nums)
+    generate_random_data(args.path,args.nums,args.batchsize)
