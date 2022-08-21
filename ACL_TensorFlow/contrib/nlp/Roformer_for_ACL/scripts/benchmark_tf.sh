51a603f3fa7e28db6628055b2b8a46176fdb7eb5
alex <alex.cheng@huawei.com>
2022-08-21 13:56:29 +0000

    update ACL_TensorFlow/contrib/nlp/Roformer_for_ACL/scripts/benchmark_tf.sh.
    Signed-off-by: alex <alex.cheng@huawei.com>

--- ACL_TensorFlow/contrib/nlp/Roformer_for_ACL/scripts/benchmark_tf.sh
+++ ACL_TensorFlow/contrib/nlp/Roformer_for_ACL/scripts/benchmark_tf.sh
@@ -12,3 +12,4 @@ rm -rf $cur_dir/$output_dir/*
 $benchmark_dir/benchmark --model $om_name --input $cur_dir/input_bins/Input-Segment,$cur_dir/input_bins/Input-Token --output $cur_dir/$output_dir
 
 #post process
+python3 run_pb_inference.py --model $cur_dir/../Roformer.pb --input $cur_dir/input_bins --npu_output $cur_dir/$output_dir --batchsize $batchsize
