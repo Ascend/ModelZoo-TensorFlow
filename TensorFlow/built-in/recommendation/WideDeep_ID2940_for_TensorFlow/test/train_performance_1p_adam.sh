ccb662604a040503dfff1ffd5733243403243504
majun121 <867479212@qq.com>
2022-08-25 09:20:17 +0000

    update WideDeep_ID2940_for_TensorFlow/test/train_performance_1p_adam.sh.
    Signed-off-by: majun121 <867479212@qq.com>

--- TensorFlow/built-in/recommendation/WideDeep_ID2940_for_TensorFlow/test/train_performance_1p_adam.sh
+++ TensorFlow/built-in/recommendation/WideDeep_ID2940_for_TensorFlow/test/train_performance_1p_adam.sh
@@ -144,7 +144,7 @@ echo "E2E Training Duration sec : $e2e_time"
 #训练用例信息，不需要修改
 BatchSize=${batch_size}
 DeviceType=`uname -m`
-CaseName=${Network}_bs${BatchSize}_${RANK_SIZE}'p'_'perf'
+CaseName=${Network}_Adam_bs${BatchSize}_${RANK_SIZE}'p'_'perf'
 
 ##获取性能数据
 #吞吐量，不需要修改
