d866ca306f456d7f2ee05ddf0a77b3ef6473eec1
wu-lanchabu <1098900762@qq.com>
2022-08-29 07:51:56 +0000

    ID2940_WideDeep新增后处理脚本
    Signed-off-by: wu-lanchabu <1098900762@qq.com>

---
+++ ACL_TensorFlow/contrib/recommendation/WideDeep_for_ACL/scripts/postprocess.py
@@ -0,0 +1,15 @@
+import os
+import argparse
+
+import numpy as np
+import tensorflow as tf
+
+
+
+
+
+if __name__="__main__":
+    parser = argparse.ArgumentParser()
+    #-----------------------------------------------------
+    parser.add_argument("--infer_result",)
+    #----------------------------------------------------
