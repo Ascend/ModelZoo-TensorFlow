1803609d438b4bc84e4690bc2044e50ced0f5fa6
xiayankang <xiayankang@outlook.com>
2022-08-31 07:06:41 +0000

    update TensorFlow/contrib/cv/Class-Splitting-GAN_ID1276_for_TensorFlow/gan_cifar_resnet_kmeans.py.
    Signed-off-by: xiayankang <xiayankang@outlook.com>

--- TensorFlow/contrib/cv/Class-Splitting-GAN_ID1276_for_TensorFlow/gan_cifar_resnet_kmeans.py
+++ TensorFlow/contrib/cv/Class-Splitting-GAN_ID1276_for_TensorFlow/gan_cifar_resnet_kmeans.py
@@ -55,10 +55,9 @@ mem = psutil.virtual_memory()
 # https://www.cs.toronto.edu/~kriz/cifar.html and fill in the path to the
 # extracted files here!
 
-parser = argparse.ArgumentParser() # 创建一个解析对象
-parser.add_argument('-d''--data_path', type=str, help='the path of dateset')
+parser = argparse.ArgumentParser() # 鍒涘缓涓€涓В鏋愬璞?parser.add_argument('--data_path', type=str, help='the path of dateset')
 args=parser.parse_args()
-# Host日志
+# Host鏃ュ織
 # os.environ['ASCEND_SLOG_PRINT_TO_STDOUT'] = "1"
 # os.environ['SLOG_PRINT_TO_STDOUT'] = "1"
 # os.environ['ASCEND_GLOBAL_LOG_LEVEL'] = "0"
@@ -360,8 +359,8 @@ custom_op = config.graph_options.rewrite_options.custom_optimizers.add()
 custom_op.name = "NpuOptimizer"
 custom_op.parameter_map["graph_memory_max_size"].s = tf.compat.as_bytes(str(15 * 1024 * 1024 * 1024))
 custom_op.parameter_map["variable_memory_max_size"].s = tf.compat.as_bytes(str(15 * 1024 * 1024 * 1024))
-config.graph_options.rewrite_options.remapping = RewriterConfig.OFF  # 必须显式关闭
-# config.graph_options.rewrite_options.memory_optimization = RewriterConfig.OFF  # 必须显式关闭
+config.graph_options.rewrite_options.remapping = RewriterConfig.OFF  # 蹇呴』鏄惧紡鍏抽棴
+# config.graph_options.rewrite_options.memory_optimization = RewriterConfig.OFF  # 蹇呴』鏄惧紡鍏抽棴
 with tf.Session(config=config) as session:
     # with tf.Session() as session:
     if softmax is None:
@@ -653,7 +652,7 @@ with tf.Session(config=config) as session:
                                             feed_dict={all_real_data_int: _data, all_real_labels: _labels,
                                                        _iteration: iteration})
 
-        lib.plot.plot('cost', _disc_cost)
+        lib.plot.plot('loss', _disc_cost)
         if CONDITIONAL and ACGAN:
             lib.plot.plot('wgan', _disc_wgan)
             lib.plot.plot('acgan', _disc_acgan)
