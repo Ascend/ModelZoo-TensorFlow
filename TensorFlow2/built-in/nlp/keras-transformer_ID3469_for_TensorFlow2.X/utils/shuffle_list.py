89c69f82816a441094fb43c68125018a8d5b28c2
chengxiang <chenxiang116@huawei.com>
2022-09-05 16:11:27 +0800

    移仓修改

--- TensorFlow2/built-in/nlp/keras-transformer_ID3469_for_TensorFlow2.X/utils/shuffle_list.py
+++
@@ -1,9 +0,0 @@
-import random
-
-random.seed(0)
-
-def shuffle_list(list):
-  shuffled = list.copy()
-  random.shuffle(shuffled)
-
-  return shuffled
--- TensorFlow2/built-in/nlp/keras-transformer_ID3469_for_TensorFlow2.X/utils/tokenize.py
+++
@@ -1,6 +0,0 @@
-from nltk import RegexpTokenizer
-
-toknizer = RegexpTokenizer(r'''\w+'|[\w-]+|[^\w\s]''')
-
-def tokenize(sentence):
-  return toknizer.tokenize(sentence)
