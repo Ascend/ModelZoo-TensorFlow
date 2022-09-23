06cd2ac8ef53e36ed57d2db80e551f78f8f3e6ca
wu-lanchabu <1098900762@qq.com>
2022-09-23 07:23:15 +0000

    update ACL_TensorFlow/contrib/recommendation/DIN_for_ACL/scripts/benchmark_tf.sh.
    Signed-off-by: wu-lanchabu <1098900762@qq.com>

--- ACL_TensorFlow/contrib/recommendation/DIN_for_ACL/scripts/benchmark_tf.sh
+++ ACL_TensorFlow/contrib/recommendation/DIN_for_ACL/scripts/benchmark_tf.sh
@@ -12,7 +12,7 @@ rm -rf $cur_dir/$output_dir/*
 $benchmark_dir/benchmark --model $om_name --input ./input_bins/pl2,./input_bins/pl3,./input_bins/pl4,./input_bins/pl5, --dymConfig dataset_conf.txt --output $output_dir 
 
 #post process
-python3 post_process.py ./ ./results  > acc.txt
+python3 postprocess.py ./ ./results  > acc.txt
 test_gauc=`grep "test_gauc:" acc.txt |awk '{print $2}'`
 test_auc=`grep "test_auc:" acc.txt |awk '{print $4}'`
 echo "test_gauc: ${test_gauc}"
