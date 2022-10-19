0581e36bacf982ab9e819bf7456f67fbdc85b94c
Victory_ztk <victoryztk1023@163.com>
2022-10-19 20:42:10 +0000

    keypose_tf2
    Signed-off-by: Victory_ztk <victoryztk1023@163.com>

---
+++ TensorFlow2/contrib/cv/KEYPOSE_ID2043_for_Tensorflow2/ckpt2pb.py
@@ -0,0 +1,40 @@
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+from tensorflow.python.tools import freeze_graph
+from tensorflow.python.framework import graph_util
+
+
+def freeze_graph(input_checkpoint,output_graph):
+
+    # checkpoint = tf.train.get_checkpoint_state(model_folder) #检查目录下ckpt文件状态是否可用
+    # input_checkpoint = checkpoint.model_checkpoint_path #得ckpt文件路径
+ 
+    # 指定输出的节点名称,该节点名称必须是原模型中存在的节点
+    # 直接用最后输出的节点，可以在tensorboard中查找到，tensorboard只能在linux中使用
+    output_node_names = "uvdw_out,uvdw_pos"
+    saver = tf.train.import_meta_graph(input_checkpoint + '.meta', clear_devices=True)
+    graph = tf.get_default_graph() # 获得默认的图
+    input_graph_def = graph.as_graph_def()  # 返回一个序列化的图代表当前的图
+ 
+    with tf.Session() as sess:
+    
+        graph_def = tf.get_default_graph().as_graph_def(add_shapes=True)
+        node_list = [n.name for n in graph_def.node]
+        for node in node_list:
+            print("node_name", node)
+        saver.restore(sess, input_checkpoint) #恢复图并得到数据
+        output_graph_def = graph_util.convert_variables_to_constants(  # 模型持久化，将变量值固定
+            sess=sess,
+            input_graph_def=input_graph_def,# 等于:sess.graph_def
+            output_node_names=output_node_names.split(","))# 如果有多个输出节点，以逗号隔开
+ 
+        with tf.gfile.GFile(output_graph, "wb") as f: #保存模型
+            f.write(output_graph_def.SerializeToString()) #序列化输出
+        print("%d ops in the final graph." % len(output_graph_def.node)) #得到当前图有几个操作节点
+
+
+        
+
+input_checkpoint='/home/keypose/savemodel/model.ckpt'
+out_pb_path='frozen_model.pb'
+freeze_graph(input_checkpoint, out_pb_path)
