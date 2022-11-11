1535248ddccfcc8eb3485c32d894f82b39d9d3d8
王凌 <wl952788888@163.com>
2022-11-11 03:15:42 +0000

    update 2Dattentionunet_ID0120_for_TensorFlow/test/train_RT2_performance_1p.sh.
    Signed-off-by: 王凌 <wl952788888@163.com>

--- TensorFlow/built-in/cv/image_segmentation/2Dattentionunet_ID0120_for_TensorFlow/test/train_RT2_performance_1p.sh
+++ TensorFlow/built-in/cv/image_segmentation/2Dattentionunet_ID0120_for_TensorFlow/test/train_RT2_performance_1p.sh
@@ -128,7 +128,7 @@ do
     
     #执行训练脚本，以下传参不需要修改，其他需要模型审视修改
     #--data_dir, --model_dir, --precision_mode, --over_dump, --over_dump_path，--data_dump_flag，--data_dump_step，--data_dump_path，--profiling，--profiling_dump_path，--autotune
-    nohup python3 mainNPU_v2_rt.py \
+    nohup python3 mainNPU_v2.py \
         --model=$model \
         --mode=$mode \
         --act=true \
