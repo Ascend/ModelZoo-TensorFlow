6eefeb43ab808060b9a0f4062be80ef652647aa6
魏利霞 <weilixia@huawei.com>
2022-11-15 09:52:14 +0000

    修改python版本为python3
    Signed-off-by: 魏利霞 <weilixia@huawei.com>

--- TensorFlow/built-in/nlp/Bert-NER_ID0797_for_TensorFlow/test/train_performance_1p.sh
+++ TensorFlow/built-in/nlp/Bert-NER_ID0797_for_TensorFlow/test/train_performance_1p.sh
@@ -113,7 +113,7 @@ do
  
     #执行训练脚本，以下传参不需要修改，其他需要模型审视修改
     #--data_dir, --model_dir, --precision_mode, --over_dump, --over_dump_path，--data_dump_flag，--data_dump_step，--data_dump_path，--profiling，--profiling_dump_path
-    nohup  python BERT_NER.py \
+    nohup  python3 BERT_NER.py \
            --precision_mode=$precision_mode \
            --task_name="NER"  \
            --do_lower_case=False \
