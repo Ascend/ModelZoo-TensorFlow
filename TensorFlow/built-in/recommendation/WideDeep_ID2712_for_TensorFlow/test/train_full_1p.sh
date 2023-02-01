8238ebc5c7eaead32c78842b7edcdb3dd141074f
limingxing517 <limingxing517@163.com>
2023-02-01 09:23:12 +0000

    fp32
    Signed-off-by: limingxing517 <limingxing517@163.com>

--- TensorFlow/built-in/recommendation/WideDeep_ID2712_for_TensorFlow/test/train_full_1p.sh
+++ TensorFlow/built-in/recommendation/WideDeep_ID2712_for_TensorFlow/test/train_full_1p.sh
@@ -27,7 +27,7 @@ display_step=10
 
 #维持参数，以下不需要修改
 over_dump=False
-
+precision_mode="allow_mix_precision"
 
 # 帮助信息，不需要修改
 if [[ $1 == --help || $1 == -h ]];then
@@ -104,7 +104,11 @@ cp configs/config.py configs/config.py.run
 cd $cur_path/../
 
 start=$(date +%s)
-nohup python3 train.py --data_path=$data_path --ckpt_path=$cur_path/output/$ASCEND_DEVICE_ID/ckpt > $cur_path/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log 2>&1 &
+nohup python3 train.py --data_path=$data_path  \
+                       --ckpt_path=$cur_path/output/$ASCEND_DEVICE_ID/ckpt \
+                       --train_size=$train_size \
+                       --precision_mode=$precision_mode \
+                       --display_step=$display_step > $cur_path/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log 2>&1 &
 wait
 end=$(date +%s)
 e2e_time=$(( $end - $start ))
