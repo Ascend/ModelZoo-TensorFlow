635a5e0d2a3dc956380c880a9dffd8244fc8760b
YakeeXv <9837540+yakeexv@user.noreply.gitee.com>
2023-03-24 03:28:45 +0000

    update InceptionV2_ID0670_for_TensorFlow/test/train_performance_8p.sh.
    Signed-off-by: YakeeXv <9837540+yakeexv@user.noreply.gitee.com>

--- TensorFlow/built-in/cv/image_classification/InceptionV2_ID0670_for_TensorFlow/test/train_performance_8p.sh
+++ TensorFlow/built-in/cv/image_classification/InceptionV2_ID0670_for_TensorFlow/test/train_performance_8p.sh
@@ -87,6 +87,9 @@ do
         cp -rf $install_path/fwkacllib/data/rl/Ascend910/custom ${autotune_dump_path}/RL/
     elif [[ $para == --data_path* ]];then
         data_path=`echo ${para#*=}`
+    elif [[ $para == --bind_core* ]]; then
+        bind_core=`echo ${para#*=}`
+        name_bind="_bindcore"
     fi
 done
 
@@ -124,12 +127,20 @@ do
         mkdir -p ${cur_path}/output/$ASCEND_DEVICE_ID/ckpt
     fi
     
+     # 绑核，不需要的绑核的模型删除，需要模型审视修改
+    corenum=`cat /proc/cpuinfo |grep "processor"|wc -l`
+    let a=RANK_ID*${corenum}/${RANK_SIZE}
+    let b=RANK_ID+1
+    let c=b*${corenum}/${RANK_SIZE}-1
 
     #执行训练脚本，以下传参不需要修改，其他需要模型审视修改
     #--data_dir, --model_dir, --precision_mode, --over_dump, --over_dump_path，--data_dump_flag，--data_dump_step，--data_dump_path，--profiling，--profiling_dump_path
-    nohup python3.7 $cur_path/../train.py --rank_size=8 \
-    --mode=train_and_evaluate \
-    --max_epochs=100 \
+    if [ "x${bind_core}" != x ];then
+        bind_core="taskset -c $a-$c"
+    fi
+    nohup ${bind_core} python3.7 $cur_path/../train.py --rank_size=8 \
+    --mode=train \
+    --max_epochs=1 \
     --T_max=100 \
     --iterations_per_loop=10 \
     --batch_size=64 \
